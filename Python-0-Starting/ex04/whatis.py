41d13afdb2bc809a43305e38cad7385cda3af5e6
alaajili <abderrahmanelaajili0@gmail.com>
2023-11-12 18:34:42 +0100

    edit whatis.py

--- Python-0-Starting/ex04/whatis.py
+++ Python-0-Starting/ex04/whatis.py
@@ -1,12 +1,17 @@
 import sys
 
+
 def whatis(num: int):
     if num % 2 == 0:
         print("I'm Even.")
     else:
         print("I'm Odd.")
 
+
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        sys.exit(1)
+
     if len(sys.argv) > 2:
         print("AssertionError: more than one argument is provided")
         sys.exit(1)
